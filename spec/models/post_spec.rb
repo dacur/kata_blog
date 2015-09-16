209ac879faedf4597ce7860383e183fc4b9323e7
dacur <dacur7@gmail.com>
2015-09-16 16:08:44 -0400

    Test for post model is passing.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:body) }
+
+  describe "valid post" do
+    subject(:post) { build(:post) }
+    it "has a title and a body" do
+      expect(post).to be_valid
+      expect(post.title).to be_a(String)
+      expect(post.body).to be_a(String)
+    end
+  end
 end
